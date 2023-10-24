a59c01bfb3c6c65fe8a21af25e7094563b7cebfe
Batuhan Taş <127934217+Batuhan-Tas@users.noreply.github.com>
2023-10-24 21:14:47 +0300

    Add files via upload

---
+++ DegiskenOlusturma.java
@@ -0,0 +1,70 @@
+package com.example.techcareerandroiddeveloperodev1.odev;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DegiskenOlusturma {
+    public static void main(String[] args) {
+        String ilce =  "Çankaya";
+        String kita = "Asya";
+        long faks = 123456789;
+        int postaKodu = 34590;
+        String instaAdresi = "https://www.instagram.com/batuhant";
+        String calistiginBölüm = "İnşaat Müh.";
+        int urunMiktarı = 50;
+        String musteriSoyadi = "Taş";
+        int odemeMiktari = 10000;
+        String dogumTarihi = "01.01.1999";
+        int borc = 50000;
+        String medeniHal = "Bekar";
+        String videoYorumu = "Süper";
+        String odemeSaati = "13:30";
+        double eftMitari = 1000000;
+        int satilanMiktar = 50;
+        String telefonModeli = "Iphone 14";
+        String dergiAdi = "The Economist";
+        String basimTarihi = "12.12.2000";
+        float zamMiktari = 4.75f;
+        int daireSayisi = 10;
+        String enlem = "35N";
+        String boylam = "45W";
+        String yemekAdi = "İmambayıldı";
+        float urunFiyat = 25.75f;
+        String sirket = "Berkshire Hathaway";
+        String videoAdi = "Paris Vlog";
+        float müzikSüresi = 3.22f;
+        int mekanPuani = 5;
+        String dosyaAdi = "Oyunlar";
+        String resimFormati = "JPEG";
+        String renk = "Black";
+        String renkKodu = "A0000C";
+        String bilgisayarModeli = "Excalibur";
+        String ekranBoyutu = "17 inç";
+        int kullanımSüresi = 1;
+        String basınc = "10 kPa";
+        String etkinlikGünü = "Cumartesi";
+        String odemeGünü = "Cuma";
+        String yolculukCikisTarihi = "24.10.2022";
+        String mahalleAdi = "Maltepe";
+        int otobusHatti = 540;
+        int kullanilanDakika = 15;
+        double kargoTakipNo = 12345678;
+        String kuponSuresi = "24 Saat";
+        String kuponKodu = "24AHsY67zT1";
+        String faturaTarihi = "24.10.2023";
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
